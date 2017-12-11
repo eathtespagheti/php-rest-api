b499a4811f918d375a7ff548618703cae67b1d28
Fabi0Z <sussarellu98@gmail.com>
2017-12-11 14:52:31 +0100

    Added delete api

--- objects/product.php
+++ objects/product.php
@@ -75,7 +75,7 @@ class Product{
           return false;
       }
   }
-  
+
   // used when filling up the update product form
   function readOne(){
 
@@ -110,6 +110,67 @@ class Product{
       $this->description = $row['description'];
       $this->category_id = $row['category_id'];
       $this->category_name = $row['category_name'];
+  }
+  // update the product
+  function update(){
+
+      // update query
+      $query = "UPDATE
+                  " . $this->table_name . "
+              SET
+                  name = :name,
+                  price = :price,
+                  description = :description,
+                  category_id = :category_id
+              WHERE
+                  id = :id";
+
+      // prepare query statement
+      $stmt = $this->conn->prepare($query);
+
+      // sanitize
+      $this->name=htmlspecialchars(strip_tags($this->name));
+      $this->price=htmlspecialchars(strip_tags($this->price));
+      $this->description=htmlspecialchars(strip_tags($this->description));
+      $this->category_id=htmlspecialchars(strip_tags($this->category_id));
+      $this->id=htmlspecialchars(strip_tags($this->id));
+
+      // bind new values
+      $stmt->bindParam(':name', $this->name);
+      $stmt->bindParam(':price', $this->price);
+      $stmt->bindParam(':description', $this->description);
+      $stmt->bindParam(':category_id', $this->category_id);
+      $stmt->bindParam(':id', $this->id);
+
+      // execute the query
+      if($stmt->execute()){
+          return true;
+      }else{
+          return false;
+      }
+  }
+  // delete the product
+  function delete(){
+
+      // delete query
+      $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+
+      // prepare query
+      $stmt = $this->conn->prepare($query);
+
+      // sanitize
+      $this->id=htmlspecialchars(strip_tags($this->id));
+
+      // bind id of record to delete
+      $stmt->bindParam(1, $this->id);
+
+      // execute query
+      if($stmt->execute()){
+          return true;
+      }
+
+      return false;
+
   }
 }
 ?>
